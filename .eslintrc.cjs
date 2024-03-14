01357c8fce22fd4a6e193337472739bb6943d5b8
ulysses-ck <ulisesapaza.280@gmail.com>
2024-03-13 23:27:53 -0300

    style(formatting): setup prettier.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,28 +1,25 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
-  extends: 'standard',
+  extends: ["standard", "plugin:prettier/recommended"],
   overrides: [
     {
       env: {
-        node: true
+        node: true,
       },
-      files: [
-        '.eslintrc.{js,cjs}'
-      ],
+      files: [".eslintrc.{js,cjs}"],
       parserOptions: {
-        sourceType: 'script'
-      }
-    }
+        sourceType: "script",
+      },
+    },
   ],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
   rules: {
-    // activate semicolon rule
-    semi: ['error', 'always']
-  }
+    "prettier/prettier": "warn",
+  },
 };
--- index.js
+++
@@ -1,4 +0,0 @@
-// run dotenvx only if not in production
-if (process.env.NODE_ENV !== 'production') {
-  require('@dotenvx/dotenvx').config();
-}
